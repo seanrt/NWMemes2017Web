b41756c19aa8995d416a0770a7072bac997be89b
Andy Tung <andyytung@gmail.com>
2017-03-19 05:27:43 -0700

    added chat

--- script.js
+++ script.js
@@ -10,4 +10,25 @@ $(function() {
         var popup = document.getElementById('popup');
         popup.style.display = 'none';
     });
+
+    var toggleChat = document.getElementById('toggle-chat');
+    toggleChat.addEventListener('click', function() {
+        if(toggleChat.style.color === 'black') {
+            toggleChat.style.color = '#3b5998';
+            $('#toggle-chat').addClass("active");
+            $('#chat').css('display', 'block');
+        } else {
+            toggleChat.style.color = 'black';
+            $('#toggle-chat').removeClass("active");
+            $('#chat').css('display', 'none');
+        }
+    });
+
+    setInterval(
+        $.get( `https://andyytung.github.io`, function(result) {
+            console.log("hi");
+            
+        }), 1000);
+
+
 });
